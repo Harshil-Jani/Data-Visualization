631d8810f1f7d3559fe98e0226116c1f837b0490
Harshil Jani <harshiljani2002@gmail.com>
2022-02-21 00:20:10 +0530

    common UI added

---
+++ script.js
@@ -0,0 +1,4 @@
+const year = new Date().getFullYear();
+const copyright = document.getElementById("copyright");
+
+copyright.innerText = `© ${year} Data Visualization Projects with D3.js by Harshil Jani`;
