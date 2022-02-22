446d5f6dadefa7bb5b8095e0c1215105cf4c097d
Harshil Jani <harshiljani2002@gmail.com>
2022-02-22 22:25:03 +0530

    Resoulution Errors'

--- Smiley/script.js
+++ Smiley/script.js
@@ -1,27 +1,27 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
 const svg = d3.select('svg')
-    .attr('width',window.innerWidth)
-    .attr('height',window.innerHeight);
+    .attr('width',2000)
+    .attr('height',2000);
 const face = svg.append('circle')
-    .attr('r', window.innerHeight/2 - 10)
-    .attr('cx',window.innerWidth/2)
-    .attr('cy',window.innerHeight/2)
+    .attr('r', 350)
+    .attr('cx',770)
+    .attr('cy',400)
     .attr('fill',"#fff34d")
     .attr('stroke','black');
 ;
 
 const leftEye = svg.append('circle')
     .attr('r', 30)
-    .attr('cx',window.innerWidth/2-150)
-    .attr('cy',window.innerHeight/2-150)
+    .attr('cx',650)
+    .attr('cy',250)
     .attr('fill',"black")
 ;
 
 const rightEye = svg.append('circle')
     .attr('r', 30)
-    .attr('cx',window.innerWidth/2 +150)
-    .attr('cy',window.innerHeight/2-150)
+    .attr('cx',900)
+    .attr('cy',250)
     .attr('fill',"black")
 ;
 
@@ -32,6 +32,6 @@ const mouth = svg.append('path')
         startAngle: Math.PI /2,
         endAngle: Math.PI * 3 / 2,
       }))
-    .attr('transform',`translate(${window.innerWidth/2},${window.innerHeight/2})`)
+    .attr('transform',`translate(770,400)`)
 ;
 
